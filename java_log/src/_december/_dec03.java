7379ddf08166b4860a0ebde432379143eb03eb7d
Administrator <Administrator@20180604-123207>
2020-12-12 00:38:00 +0900

    소수점 자리 표현

---
+++ java_log/src/_december/_dec03.java
@@ -0,0 +1,13 @@
+package _december;
+
+public class _dec03 {
+	public static void main(String[] args) {
+		float f = 1.2345f;
+		
+		//소수점 자리수 미지정시
+		System.out.printf("소수점 자리수 미지정: %f%n",f);
+		//소수점 자리 지정시
+		System.out.printf("소수점 자리수 지정: %.3f%n",f);
+	}
+
+}
