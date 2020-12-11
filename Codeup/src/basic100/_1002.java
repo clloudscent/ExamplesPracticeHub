144bce019b269233f4717fae5336f7a74719fcd7
Administrator <Administrator@20180604-123207>
2020-12-11 22:31:02 +0900

    Codeup

---
+++ Codeup/src/basic100/_1002.java
@@ -0,0 +1,8 @@
+package basic100;
+
+public class _1002 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Codeup/src/basic100/_1011.java
@@ -0,0 +1,12 @@
+package basic100;
+
+import java.io.IOException;
+
+public class _1011 {
+	public static void main(String[] args) throws IOException {
+		char x;
+		x = (char)System.in.read();
+		System.out.println(x);
+				
+	}
+}
