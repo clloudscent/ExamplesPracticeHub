9ae22ce115b0789e669e0b2074591738670c339c
Administrator <Administrator@20180604-123207>
2020-12-15 09:43:48 +0900

    .

--- class_review/src/class_review/ex08.java
+++ class_review/src/class_review/ex08.java
@@ -1,17 +1,15 @@
 package class_review;
-//if문 예제
 
-public class ex07 {
+public class ex08 {
 	public static void main(String[] args) {
-		int score = 93;
+		int score =85;
 		
 		if(score>=90) {
 			System.out.println("점수가 90보다 큽니다");
 			System.out.println("등급은 A입니다");
-		}
-		if(score<90)
+		} else {
 			System.out.println("점수가 90보다 작습니다");
-		System.out.println("등급은 B입니다");//if문과는 상관없는 실행문
+			System.out.println("등급은 B입니다");
+		}
 	}
 }
-
