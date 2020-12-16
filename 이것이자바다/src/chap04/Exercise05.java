155065019b7cfbba01d0877a0149f4d94c323f0c
Administrator <Administrator@20180604-123207>
2020-12-16 15:09:53 +0900

    .

--- 이것이자바다/src/chap04/Exercise05.java
+++ 이것이자바다/src/chap04/Exercise05.java
@@ -1,5 +1,15 @@
 package chap04;
-
+//for 문을 이용해 방정식 4x+5y=60의 모든 해를 구해서 (x,y)형태로 출력
+//x,y는 자연수
 public class Exercise05 {
-
+	public static void main(String[] args) {
+		for(int x=1; x<=10; x++) {
+			for(int y=1; y<=10; y++) {
+				if((4*x + 5*y) ==60){
+					System.out.println("(" + x + "." + y + ")");
+					
+				}
+			}
+		}		
+	}
 }
