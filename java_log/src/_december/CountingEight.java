ddc22c205c0cfd225bc5887ef5cc7551b9c54d0e
Administrator <Administrator@20180604-123207>
2020-12-18 18:48:02 +0900

    .

--- java_log/src/_december/CountingEight.java
+++ java_log/src/_december/CountingEight.java
@@ -2,18 +2,15 @@ package _december;
 
 public class CountingEight {
 	public static void main(String[] args) {
-		for (int i = 0; i <= 10000; i++) {
+		for (int i=0; i<=10000; i++) {
 			searchEight(i);
 		}
-		System.out.println("1에서 10,000 사이에 존재하는 8의 개수는? " + count);
+		System.out.println("1에서 10000사이에 존재하는 8의 갯수?"+count);
 	}
-
-	private static int count = 0;
-
+	private static int count =0;
 	public static void searchEight(int num) {
-		if (num % 10 == 8)
-			count++;
-		if (num > 10)
-			searchEight(num / 10);
+		if (num%10==8) count++;//?
+		if (num>10) searchEight(num/10);
 	}
 }
+
