bde1055d82aae0647a1f7754373afb0e49513976
Administrator <Administrator@20180604-123207>
2020-12-24 10:28:56 +0900

    .

--- Codeup/src/basic100/_1019.java
+++ Codeup/src/basic100/_1019.java
@@ -2,17 +2,16 @@ package basic100;
 
 import java.util.Scanner;
 
+
 public class _1019 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String a = sc.next();
-		
-		String date[] = a.split("[.]");
-		//split[]을 이용해 .(닷)을 기준으로 문자열을 잘라 배열에 저장
-		
-		int year = Integer.parseInt(date[0]);//?문자열을 숫자로 변환시킴
-		
-		
+		String[] arr = a.split("\\.");
+		int A = Integer.valueOf(arr[0]);
+		int B = Integer.valueOf(arr[1]);
+		int C = Integer.valueOf(arr[2]); //Integer은 null값 까지 표시
+		System.out.printf("%04d.%02d.%02d",A,B,C);
 		
 	}
 
